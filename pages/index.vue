c80ff832998603424cf7d444705c87cc373ab91c
AleksandrovArtyom <artyomik14@gmail.com>
2022-09-30 00:04:00 +0300

    Init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img class="avatar" :src="avatar"/>
+    <img class="avatar" :src="avatarRelative"/>
+  </div>
+</template>
+
+<script setup>
+import avatar from "@/assets/avatar.jpg";
+import avatarRelative from "../assets/avatar.jpg";
+</script>
+
+<style>
+.avatar {
+  width: 100px;
+}
+</style>
